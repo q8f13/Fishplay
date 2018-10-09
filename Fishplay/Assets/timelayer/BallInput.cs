f2d38357d8e478df56708d97e1c732e6b062530e
q8f13 <dustfall@gmail.com>
2018-10-09 10:44:05 +0800

    时间分层做法与物理的关系测试

---
+++ Fishplay/Assets/timelayer/BallInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInput : MonoBehaviour {
+
+	public float ForceScale = 10.0f;
+	public UnityEngine.UI.Text Console;
+
+	private Rigidbody _rig;
+
+	private Clock _clock;
+
+	// Use this for initialization
+	void Start () {
+		_rig = GetComponent<Rigidbody>();
+		Console.text = "";
+		_clock = FindObjectOfType<Clock>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_clock.Scale < 1.0f)
+			return;
+
+		if(Input.GetKeyUp(KeyCode.W))
+			InputForceAndDisplay(KeyCode.W, Vector3.up);
+		else if(Input.GetKeyUp(KeyCode.S))
+			InputForceAndDisplay(KeyCode.S, Vector3.down);
+		else if(Input.GetKeyUp(KeyCode.A))
+			InputForceAndDisplay(KeyCode.A, Vector3.left);
+		else if(Input.GetKeyUp(KeyCode.D))
+			InputForceAndDisplay(KeyCode.D, Vector3.right);
+	}
+
+	void InputForceAndDisplay(KeyCode kcode, Vector3 direction)
+	{
+		_rig.AddForce(direction * ForceScale, ForceMode.Impulse);
+		Console.text += kcode.ToString() + "\n";
+	}
+}
---
+++ Fishplay/Assets/timelayer/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	public Rigidbody Ball;
+	[Range(0.0f, 1.0f)]
+	public float Scale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		Physics.autoSimulation = false;
+	}
+	
+	void FixedUpdate()
+	{
+		Physics.Simulate(Time.fixedDeltaTime * Scale);
+	}
+
+	private void OnGUI()
+	{
+		Scale = GUI.HorizontalSlider(new Rect(Screen.width - 140, Screen.height - 40,140, 40)
+			, Scale
+			, 0.0f
+			, 1.0f)	;
+	}
+}
